59d79a2bbfa77cdee6f9d9f6c9b6f4040f7a2fca
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2022-09-14 15:20:54 +0300

    fix configuration

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Multi-Cloud, Multi-Region Database Service',
-    illustration: '/img/database.png',
+    illustration: 'img/database.png',
     description: (
       <>
         Deploy your applications to multiple cloud platforms across multiple regions, including Google Cloud. 
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Fully-Integrated Developer Data Platform',
-    illustration: '/img/app-services.png',
+    illustration: 'img/app-services.png',
     description: (
       <>
         Build and run production-ready apps on top of Atlas in a fraction of the time with fully managed cloud services such as Functions, Triggers, and APIs.
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'AI/ML Capabilities',
-    illustration: '/img/analytics.png',
+    illustration: 'img/analytics.png',
     description: (
       <>
         Leverage the Google Cloud Natural Language API to analyze text and extract business insights.
